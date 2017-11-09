48f728533fe8a044c227ba0dec0613db8c060b81
ScottB <17sb@cis.dk>
2017-11-09 19:13:43 +0100

    Add jQuery to Select Element

--- spec/bmi_ui_spec.js
+++ spec/bmi_ui_spec.js
@@ -1,15 +1,57 @@
-  //
-  //
-  //
+  describe('BMI Imperial', function() {
+    beforeEach(function() {
+      jasmine.getFixtures().fixturesPath = '.';
+      loadFixtures('index.html');
+      $.holdReady(false);
+      $('#weight').val('176');
+      $('#height').val('6 feet 3 inches');
+      $('#calculate').trigger('click');
+    });
+
+    it("displays BMI Value Imperial Method", function() {
+      expect($('#display-value').text()).toBe('Your BMI is 22');
+    });
+
+    it("displays BMI Message Imperial Method", function() {
+      expect($('#display-message').text()).toBe('and you are Normal')
+    });
+
+    describe("BMI Imperial - Height in Inches", function() {
+      beforeEach(function() {
+        jasmine.getFixtures().fixturesPath = '.';
+        loadFixtures('index.html');
+        $.holdReady(false);
+        $('#BMI-Method').val('metric');
+        $('#weight').val('176');
+        $('#height').val('75');
+        $('#calculate').trigger('click');
+      });
+
+      it("displays BMI Value Imperial Method", function() {
+        expect($('#display-value').text()).toBe('Your BMI is 22');
+      });
+
+      it("displays BMI Message Imperial Method", function() {
+        expect($('#display-message').text()).toBe('and you are Normal');
+      });
+    });
+  });
+
+
   describe('BMI Metric', function() {
     beforeEach(function() {
       jasmine.getFixtures().fixturesPath = '.';
       loadFixtures('index.html');
       $.holdReady(false);
-      $('#metric-weight').val('90');
-      $('#metric-height').val('186');
-      $('#metric-calculate').trigger('click');
+      $('#weight').val('90');
+      $('#height').val('186');
+      $('#BMI-Method option').filter(function() {
+        return ($(this).text() == 'metric');
+      }).prop('selected', true);
+      // $('#BMI-Method').val('metric');
+      $('#calculate').trigger('click');
     });
+
 
     it("display BMI Value", function() {
       expect($('#display-value').text()).toBe('Your BMI is 26.01');
--- src/imperial_calculator.js
+++ src/imperial_calculator.js
@@ -24,21 +24,21 @@ weight = parseInt(weight) * 703;
   if (weight > 0 && height > 0) {
     var finalBmi = weight / (height * height);
     obj.bmiValue = parseFloat(finalBmi.toFixed(2));
-    setBMIMessage(obj);
+    setIMPBMIMessage(obj);
   }
 };
 
-function setBMIMessage (obj) {
+function setIMPBMIMessage (obj) {
   if (obj.bmiValue < 18.5) {
     obj.bmiMessage = "Underweight"
   }
-  if (obj.bmiValue > 18.5 && obj.bmiValue < 25) {
+  if (obj.bmiValue >= 18.5 && obj.bmiValue < 25) {
     obj.bmiMessage = "Normal"
   }
-  if (obj.bmiValue > 25 && obj.bmiValue < 30) {
+  if (obj.bmiValue >= 25 && obj.bmiValue < 30) {
     obj.bmiMessage = "Overweight"
   }
-  if (obj.bmiValue > 30) {
+  if (obj.bmiValue >= 30) {
     obj.bmiMessage = "Obese"
   }
 }
